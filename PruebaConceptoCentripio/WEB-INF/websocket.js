7c61f4b8c9cc119c752323ade5113dd0bc53cc8c
pepe <pepe@PEPE-PC.local>
2021-02-13 15:57:18 -0200

    Actualizar websocket.js y PushTimeService.java

--- PruebaConceptoCentripio/WEB-INF/websocket.js
+++ PruebaConceptoCentripio/WEB-INF/websocket.js
@@ -1,4 +1,4 @@
-var socket = new WebSocket("ws://localhost:8080/ProgressWebSocket-1.0-SNAPSHOT/progress");
+var socket = new WebSocket("ws://localhost:8080/ProgressWebSocket/progress");
 socket.onmessage = onMessage;
 
 function onMessage(event) {
