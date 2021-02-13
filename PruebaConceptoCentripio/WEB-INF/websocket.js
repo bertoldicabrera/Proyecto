a6221b6d7231edf1903b214c957399ca7fe6db48
Joaquin Segovia <joasegovia9427@gmail.com>
2021-02-13 18:06:09 -0300

    ajuste prc

--- PruebaConceptoCentripio/WEB-INF/websocket.js
+++ PruebaConceptoCentripio/WEB-INF/websocket.js
@@ -1,9 +1,16 @@
-var socket = new WebSocket("ws://localhost:8080/ProgressWebSocket-1.0-SNAPSHOT/progress");
+console.log("¡Detente!");
+console.log("Esta función del navegador está pensada para desarrolladores. Si alguien te indicó que copiaras y pegaras algo aquí para habilitar una función para 'hackear', se trata de un fraude. Si lo haces, esta persona podrá acceder a tu cuenta.");
+
+console.log("WARNING!!!");
+console.log("This browser feature is for developers only. Please do not copy-paste any code or run any scripts here. It may cause your account to be compromised.");
+
+
+var socket = new WebSocket("ws://localhost:8080/prueba/endpoint");
 socket.onmessage = onMessage;
 
 function onMessage(event) {
     var btnSubmit = document.getElementById("btnSubmit");
-    btnSubmit.disabled = true;
+    //btnSubmit.disabled = true;
     
     var progress = document.getElementById("progress");
     var data = JSON.parse(event.data);
@@ -20,7 +27,42 @@ function onMessage(event) {
 }
 
 function formSubmit() {
-    socket.send("{\"start\":\"true\"}");
+	if(!document.querySelector('input[name="partida"]:checked')) {
+      alert('Error, seleccione una partida');
+
+    }
+	else{
+	
+		var partida = document.querySelector('input[name="partida"]:checked');
+		console.log(partida);
+		console.log(document.querySelector('input[name="partida"]:checked'));
+
+		console.log(partida.value);
+
+
+		socket.send(partida.value );
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
